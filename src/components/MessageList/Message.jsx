8b3f647878e0cafd7f93846db74c29ae918ce2f5
Victor De Ponte <rdbvictor19@gmail.com>
2017-03-16 18:51:51 -0300

    Preview added to images and pdf files.

--- src/components/MessageList/Message.jsx
+++ src/components/MessageList/Message.jsx
@@ -3,13 +3,31 @@ import classNames from 'classnames';
 import { Component } from 'react';
 import { kebabCase } from 'lodash/string';
 import Linkify from 'react-linkify';
+import Lightbox from 'react-images';
 import moment from 'moment';
 import $ from 'jquery';
 
 export default class Message extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { lightboxIsOpen: false };
+        this.openLightbox.bind(this);
+        this.closeLightbox.bind(this);
+    }
+
     componentDidMount(prevProps, prevState) {
         $(".chattigo-message span.Linkify a").attr( "target", "_blank" );
     }
+
+    openLightbox(content_type) {
+        if (content_type !== "application/pdf")
+            this.setState({ lightboxIsOpen: true });
+    }
+
+    closeLightbox() {
+        this.setState({ lightboxIsOpen: false });
+    }
+
     render(){
         const message = this.props.message;
         const settings = this.context.settings;
@@ -26,8 +44,42 @@ export default class Message extends Component {
                         {(() => {
                                 if (message.attachments) {
                                     return <div className={"chattigo-message-attachments"}>
+                                                <Lightbox
+                                                    images={message.attachments.map((attachment) => {
+                                                        if (attachment.content_type === "application/pdf") {
+                                                            return null;
+                                                        }
+                                                        return {
+                                                            src: attachment.url.original,
+                                                            thumbnail: attachment.url.thumbnail,
+                                                            caption: attachment.name
+                                                        };
+                                                    })}
+                                                    isOpen={this.state.lightboxIsOpen}
+                                                    onClose={() => {this.closeLightbox()}}
+                                                />
                                                 {message.attachments.map((attachment) => {
-                                                    return <img key={attachment.name} src={attachment.url.thumbnail} alt={attachment.name}/>;
+                                                    if (attachment.content_type === "application/pdf") {
+                                                        return <a
+                                                                    key={attachment.name}
+                                                                    title={attachment.name}
+                                                                    href={attachment.url.original}
+                                                                    target="_blank"
+                                                                >
+                                                                    <img
+                                                                        src={attachment.url.thumbnail}
+                                                                        alt={attachment.name}
+                                                                        onClick={() => {this.openLightbox(attachment.content_type)}}
+                                                                        />
+                                                                </a>;
+                                                    } else {
+                                                        return <img
+                                                            key={attachment.name}
+                                                            src={attachment.url.thumbnail}
+                                                            alt={attachment.name}
+                                                            onClick={() => {this.openLightbox(attachment.content_type)}}
+                                                            />;
+                                                    }
                                                 })}
                                            </div>;
                                 }
